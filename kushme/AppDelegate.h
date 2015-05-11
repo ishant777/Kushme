2ade49b50c8a1e41b2bdde13cf28e884e4d75c8b
Paras Gorasiya <parasgorasiya@Parass-MacBook-Pro.local>
2015-05-11 23:24:16 +0530

    Initial Commit

---
+++ kushme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kushme
+//
+//  Created by Paras Gorasiya on 11/05/15.
+//  Copyright (c) 2015 Paras Gorasiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
